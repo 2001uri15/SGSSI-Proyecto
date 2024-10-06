d5ace2f3f023036d16289464966f590e12bf4b50
Aritz Blasco <games.aritz@gmail.com>
2024-10-06 14:03:16 +0200

    Creacion rama entrega_1

--- app/login.php
+++ app/login.php
@@ -1,9 +1,11 @@
 <?php
 require_once 'plantillas/header.php'; // Incluimos el header
 ?>
+
 <div class="body-margen">
     <div class="login">
         <h1>Iniciar Sesión</h1>
+        <!-- Mostrar un mensaje de error si la contraseña o el usuario son incorrectos -->
         <form id="login_form" class="login-form" action="process_login.php" method="post">
             <label class="login-label" for="username">Nombre de Usuario:</label>
             <input class="login-input" type="text" id="username" name="username" required>
@@ -17,7 +19,21 @@ require_once 'plantillas/header.php'; // Incluimos el header
         </form>
     </div>
 </div>
+
+<script>
+    // Comprobar si hay un error en la URL y mostrar un alert
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('error')) {
+        const error = urlParams.get('error');
+        if (error === 'incorrect_password') {
+            alert('La contraseña es incorrecta. Inténtalo de nuevo.');
+        } else if (error === 'user_not_found') {
+            alert('El usuario no existe. Revisa tu nombre de usuario o regístrate.');
+        }
+    }
+</script>
+
 <?php
-require_once 'plantillas/footer.php';
+require_once 'plantillas/footer.php'; // Incluimos el footer
 ?>
 
