a537c9f63fdbfd84c18d7b0b46d80bd0131bb7ca
acotanor <acotanor@gmail.com>
2024-11-13 17:46:23 +0100

    Sanitización para prevenir XSS.

--- app/process_add_item.php
+++ app/process_add_item.php
@@ -10,18 +10,18 @@
 
     ob_start(); // Inicia el buffer de salida
     if(isset($_POST['item_add_submit'])) {
-        $matricula = $_POST['matricula'];
-        $modelo = $_POST['modelo'];
-        $marca = $_POST['marca'];
-        $tipo_combustion = $_POST['tipo_combustion'];
-        $color = $_POST['color'];
-        
+        // Recive y sanitiza los datos del formulario.
+        $matricula = preg_replace('/[^a-zA-Z0-9-]/', '', (string)$_POST['matricula']);
+        // matricula tiene un regex distinto para permitir -.
+        $modelo = preg_replace('/[^a-zA-Z0-9]/', '', (string)$_POST['modelo']);
+        $marca = preg_replace('/[^a-zA-Z0-9]/', '', (string)$_POST['marca']);
+        $tipo_combustion = preg_replace('/[^a-zA-Z0-9]/', '', (string)$_POST['tipo_combustion']);
+        $color = preg_replace('/[^a-zA-Z0-9]/', '', (string)$_POST['color']);
 
         // Inserta los datos en la base de datos
         $sql = "INSERT INTO `coches` (`matricula`, `tipo_combustion`, `modelo`, `color`, `marca`) 
                 VALUES ('$matricula', '$tipo_combustion', '$modelo', '$color', '$marca')";
 
-        
         // Ejecuta la consulta y verifica si fue exitosa
         if ($conn->query($sql) === TRUE) {
             header('Location: /items.php');
@@ -31,3 +31,4 @@
         }
     }
     ob_end_flush(); // Envía el contenido del buffer al navegador
+?>
